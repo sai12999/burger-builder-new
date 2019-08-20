71336a67aa632189466c94e939e40caddf416645
sai12999 <sai12999.k@gmail.com>
2019-08-20 19:04:18 +0530

    "first commit"

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl'
+
+const BuildControls = (props) =>{
+    return (
+        <div className={classes.BuildControls}>
+        
+        </div>
+    )
+}
+
+export default BuildControls
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
+import classes from './Burger.css'
+
+const burger = (props) => {
+
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) =>
+            <BurgerIngredients key={igKey + i} type={igKey} />
+        )
+    }).reduce((arr, curr) => {
+        return arr.concat(curr)
+    }, [])
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please Start Adding Ingredients</p>
+    }
+    
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredients type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredients type="bread-bottom" />
+        </div>
+    )
+}
+
+export default burger
