ab753851574464b85f6ef8c6393bc621cdfc1a4b
sai12999 <sai12999.k@gmail.com>
2019-08-30 12:22:17 +0530

    Updates

--- precache-manifest.d64012a06fb582985693d95ba54f2c42.js
+++ precache-manifest.d64012a06fb582985693d95ba54f2c42.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "adc8811e49e9bd5a2c5788440d675dbd",
+    "revision": "acbaf1a3d57bb8db4881a2c684a5616f",
     "url": "/burger-builder-new/index.html"
   },
   {
-    "revision": "eeeaf53367a1eaecab2a",
-    "url": "/burger-builder-new/static/css/main.4f39383b.chunk.css"
+    "revision": "3b16608f3b486c1b5a61",
+    "url": "/burger-builder-new/static/css/main.44a79082.chunk.css"
   },
   {
     "revision": "a55efc12d0bc844dcfc2",
     "url": "/burger-builder-new/static/js/2.01c3c802.chunk.js"
   },
   {
-    "revision": "eeeaf53367a1eaecab2a",
-    "url": "/burger-builder-new/static/js/main.7c752fa9.chunk.js"
+    "revision": "3b16608f3b486c1b5a61",
+    "url": "/burger-builder-new/static/js/main.edce1003.chunk.js"
   },
   {
     "revision": "f832cd7fc326ef298450",
